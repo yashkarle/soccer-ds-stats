2e77783864e5287ae76960655f176ac6405621d9
Yash Karle <yashkarle@gmail.com>
2022-09-05 23:53:56 +0100

    tut 2 and jorgi passes linting plus colors

--- Projects/4UppsalaMMS/1_PlottingActions/Jorginho_Euros_Plots.py
+++ Projects/4UppsalaMMS/1_PlottingActions/Jorginho_Euros_Plots.py
@@ -1,1 +1,66 @@
-import matplotlib.pyplot as pltimport numpy as npfrom mplsoccer import Pitch, Sbopenparser = Sbopen()df_competitions = parser.competition()df_matches = parser.match(competition_id=55, season_id=43)# Italy matches: [3788741, 3788754, 3788766, 3794685, 3795107, 3795220, 3795506]# Italy matches: [Turkey, England]df, related, freeze, tactics = parser.event(3788741)passes = df.loc[df['type_name'] == 'Pass'].loc[    df['sub_type_name'] != 'Throw-in'].set_index('id')# Jorginho name: Jorge Luiz Frello Filho# Jorgingo player_id: 7024mask_jorgi= (df.type_name == 'Pass') & (df.player_name ==                                         "Jorge Luiz Frello Filho")df_pass = df.loc[mask_jorgi, ['x', 'y', 'end_x', 'end_y']]pitch = Pitch(line_color='black')fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,                     endnote_height=0.04, title_space=0, endnote_space=0)fig.suptitle("Jorginho against Turkey", fontsize = 30)pitch.arrows(df_pass.x, df_pass.y,            df_pass.end_x, df_pass.end_y, color = "blue", ax=ax['pitch'])pitch.scatter(df_pass.x, df_pass.y, alpha = 0.2, s = 500,               color = "blue", ax=ax['pitch'])plt.show()
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  5 23:49:41 2022
+
+@author: yashkarle
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from mplsoccer import Pitch, Sbopen
+
+parser = Sbopen()
+
+df_competitions = parser.competition()
+df_matches = parser.match(competition_id=55, season_id=43)
+# Italy matches: [3788741, 3788754, 3788766, 3794685, 3795107, 3795220, 3795506]
+# Italy matches: [Turkey, Switzerland, Wales, Austria, Belgium, Spain, England]
+
+df, related, freeze, tactics = parser.event(3795107)
+passes = df.loc[df['type_name'] == 'Pass'].loc[
+    df['sub_type_name'] != 'Throw-in'].set_index('id')
+
+# Jorginho name: Jorge Luiz Frello Filho
+# Jorgingo player_id: 7024
+mask_jorgi_back = ((df.type_name == 'Pass') & (df.player_name ==
+                                               "Jorge Luiz Frello Filho")
+                   & (df.end_x < df.x)
+                   & (~df.outcome_id.isin([9, 75, 76])))
+mask_jorgi_front = ((df.type_name == 'Pass') & (df.player_name ==
+                                                "Jorge Luiz Frello Filho")
+                    & (df.end_x > df.x)
+                    & (df.pass_length <= 25)
+                    & (~df.outcome_id.isin([9, 75, 76])))
+mask_jorgi_front_prog = ((df.type_name == 'Pass') & (df.player_name ==
+                                                     "Jorge Luiz Frello Filho")
+                         & (df.end_x > df.x)
+                         & (df.pass_length > 25)
+                         & (~df.outcome_id.isin([9, 75, 76])))
+
+df_pass_back = df.loc[mask_jorgi_back, ['x', 'y', 'end_x', 'end_y']]
+df_pass_front = df.loc[mask_jorgi_front, ['x', 'y', 'end_x', 'end_y']]
+df_pass_front_prog = df.loc[mask_jorgi_front_prog,
+                            ['x', 'y', 'end_x', 'end_y']]
+
+pitch = Pitch(line_color='black')
+fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                     endnote_height=0.04, title_space=0, endnote_space=0)
+fig.suptitle("Jorginho against Belgium", fontsize=30)
+
+#pitch.arrows(df_pass_front_prog.x, df_pass_front_prog.y,
+# df_pass_front_prog.end_x, df_pass_front_prog.end_y,
+#color = "green", ax=ax['pitch'])
+pitch.scatter(df_pass_front_prog.x, df_pass_front_prog.y, alpha=0.2, s=250,
+              color="green", ax=ax['pitch'])
+
+#pitch.arrows(df_pass_front.x, df_pass_front.y, df_pass_front.end_x,
+# df_pass_front.end_y, color = "blue", ax=ax['pitch'])
+pitch.scatter(df_pass_front.x, df_pass_front.y, alpha=0.2, s=250,
+              color="blue", ax=ax['pitch'])
+
+#pitch.arrows(df_pass_back.x, df_pass_back.y, df_pass_back.end_x,
+#df_pass_back.end_y, color = "red", ax=ax['pitch'])
+pitch.scatter(df_pass_back.x, df_pass_back.y, alpha=0.2, s=250,
+              color="red", ax=ax['pitch'])
+
+plt.show()
